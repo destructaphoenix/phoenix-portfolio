c360c2efaf6aeb6a256986552219771d93d01dd3
rsphoenix02 <saq.saf0501@gmail.com>
2026-05-12 11:16:14 +0530

    Project setup + initial hero section

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import Hero from "./sections/Hero"
+
+const App = () => {
+  return (
+    <>
+      <Hero></Hero>
+    </>
+  )
+}
+
+export default App
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,57 @@
+import Button from "../components/Button"
+import HeroExperience from "../components/HeroModels/HeroExperience"
+import { words } from "../constants/index"
+
+const Hero = () => {
+  return (
+    <section id="hero" className="relative overflow-hidden">
+        <div className="absolute top-0 left-0 z-10">
+            <img src="/images/bg.png" alt="background"></img>
+        </div>
+
+        <div className="hero-layout">
+            {/* Left: Hero content */}
+            <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
+                <div className="flex flex-col gap-7">
+                    <div className="hero-text">
+                        <h1>Shaping
+                            <span className="slide">
+                                <span className="wrapper">
+                                    {words.map((word) => (
+                                        <span key={word.text} 
+                                            className="flex items-center md:gap-3 gap-1 pb-2">
+                                            <img src={word.imgPath}
+                                            alt={word.text}
+                                            className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50" />
+                                            <span>{word.text}</span>
+                                        </span>
+                                    ))}
+                                </span>
+                            </span>
+                        </h1>
+                        <h1>into Real Projects</h1>
+                        <h1>that Deliver Results</h1>
+                    </div>
+                    <p className="text-white-50 md:text-xl relatice z-10 pointer-events-none">
+                        Hi, I'm Phoenix. A developer.
+                    </p>
+                    <Button 
+                        className="md:w-80 md:h-16 w-60 h-12"
+                        id="button"
+                        text="See my work"
+                    ></Button>
+                </div>
+            </header>
+
+            {/* Right: 3D model */}
+            <figure>
+                <div className="hero-3d-layout">
+                    <HeroExperience></HeroExperience>
+                </div>
+            </figure>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
